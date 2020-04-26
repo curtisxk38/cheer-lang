ec605efe85e7a26507f2a2fd9f68b15671bf2028
curtisxk38 <ck2ha@virginia.edu>
2020-04-26 17:41:23 -0400

    Remove unused code

--- cheer/gen_ir.py
+++ cheer/gen_ir.py
@@ -249,10 +249,7 @@ class CodeGenVisitor(visit.DFSVisitor):
         op1 = self.exp_stack.pop()
         self.add_line(f"%{self.reg_num} = icmp eq i32 %{op1.name}, %{op2.name}")
         self.exp_stack.append(Var(self.reg_num, "i1"))
-        self.reg_num += 1
-        #self.add_line(f"%{self.reg_num} = zext i1 %{self.reg_num - 1} to i32")
-        
-        #self.reg_num += 1
+        self.reg_num += 
 
     def _out_plus_exp(self, node):
         op2 = self.exp_stack.pop()
